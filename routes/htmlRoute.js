bd60f3211ec9f95487af2fbc560dc595374075c5
Ravi Joshi <ravipiyushjoshi@gmail.com>
2019-08-09 13:58:02 -0400

    Updated file as required to changes to display in the function

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -1,21 +1,28 @@
 var path = require("path");
 
 
+/*app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+  });
+  
+  app.get("/add", function(req, res) {
+    res.sendFile(path.join(__dirname, "add.html"));
+  });
+  
+*/
+
 
 module.exports = function(app){
 
-    app.get("/",function(req,res){
-        res.sendFile(path.join(__dirname,"index.html"));
-
-    });
+   
     app.get("/tables", function(req, res) {
-        res.sendFile(path.join(__dirname, "tables.html"));
+        res.sendFile(path.join(__dirname, "../public/tables.html"));
       });
       
-      app.get("/reservation", function(req, res) {
-        res.sendFile(path.join(__dirname, "reservation.html"));
+      app.get("/reserve", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/reserve.html"));
       });
       app.get("/*",function(req,res){
-        res.sendFile(path.join(__dirname,"index.html"))
+        res.sendFile(path.join(__dirname,"../public/index.html"));
       });
 };
